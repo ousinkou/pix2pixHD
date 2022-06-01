debb98dd42e19223cd01230b7c4700766a8d9737
Zhenxing Wang <wangzx@4dstc.com>
2022-06-01 16:46:54 +0800

    Update predict

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 import torch
 from PIL import Image, ImageFilter
-import cv2
+from skimage import morphology, feature
+from skimage.segmentation import flood_fill
 import torchvision.transforms as transforms
 from models.pix2pixHD_model import InferenceModel
 import numpy as np
@@ -18,85 +19,125 @@ def tensor2im(image_tensor, imtype=np.uint8, normalize=True):
         image_numpy = image_numpy[:,:,0]
     return image_numpy.astype(imtype)
 
-def sketch2edge(sketch_pil, mask_pil):
-    bold_factor = 5
-    sk_new = sketch_pil.filter(ImageFilter.MinFilter(size=bold_factor))
-    sk_arr = np.array(sketch_pil) > 128
-    sk_new_arr = np.array(sk_new) > 128
-    sk_new2_arr = ((~sk_new_arr) & sk_arr).astype(np.uint8)
-    mask_bin = (np.array(mask_pil) > 128).astype(np.uint8)
-    mask =  mask_bin*128 + (1-mask_bin)*255
-    #Image.fromarray(mask).save("./tmp1.png")
-    #Image.fromarray(sk_new2_arr*255).save("./tmp2.png")
-    img_arr = mask*sk_new2_arr
-    return Image.fromarray(img_arr)
 
-#img_path = "/data/Dataset/sketch_test/sketch_test3/sketch/4.jpg"
-#mask_path = "/data/Dataset/sketch_test/sketch_test3/mask/4.jpg"
-#img_path = "/data/Dataset/sketch_test/new/sketch/54733238_a6bd9d22714887c8cec4e52b25c5d621_0000_4.png"
-#mask_path = "/data/Dataset/sketch_test/new/mask/54733238_a6bd9d22714887c8cec4e52b25c5d621_0000_4.png"
-
-model = InferenceModel()
-opt = Namespace(name='sketchshade0524',
-                gpu_ids=[0],
-                checkpoints_dir='./checkpoints',
-                model='pix2pixHD',
-                norm='instance',
-                use_dropout=False,
-                data_type=16,
-                verbose=False,
-                fp16=False,
-                local_rank=0,
-                batchSize=1, loadSize=800, fineSize=800, label_nc=0, input_nc=1, output_nc=1,
-                resize_or_crop='resize', serial_batches=True, netG='global', ngf=64, n_downsample_global=4,
-                n_blocks_global=9, n_blocks_local=3, n_local_enhancers=1, niter_fix_global=0,
-                no_instance=True, instance_feat=False, label_feat=False, feat_num=3, load_features=False,
-                n_downsample_E=4, nef=16, n_clusters=10, aspect_ratio=1.0, phase='test', which_epoch='latest', how_many=-1,
-                cluster_path='features_clustered_010.npy', use_encoded_image=False,  isTrain=False)
-model.initialize(opt)
-model.half()
-
-exp_name = "sketech_test4"
-os.makedirs(f"image/{exp_name}", exist_ok=True)
-
-base_path = f"/data/Dataset/sketch_test/{exp_name}"
-imgs = os.listdir(f"{base_path}/sketch")
-#base_path = "/data/Dataset/sketch_test/sketch_test_white"
-#imgs = os.listdir(f"{base_path}/raw")
-
-for img in imgs:
-    img_path = f"{base_path}/sketch/{img}"
-    mask_path = f"{base_path}/mask/{img}"
-    img_pil = Image.open(img_path).convert('L')
-    mask_pil = Image.open(mask_path).convert('L')
-
-    """
-    img_path = f"{base_path}/raw/{img}"
-    img_pil = Image.open(img_path)
-    img_pil.getchannel("A").save(f"{base_path}/mask/{img}")
-    img_pil.convert("L").save(f"{base_path}/sketch/{img}")
-    continue
-    """
-
-    sk2edge_pil = sketch2edge(img_pil, mask_pil)
-    #sk2edge_pil.save("./image/sk2edge_pil.png")
-
-    trans = transforms.Compose([transforms.Resize([800, 800], Image.BICUBIC),
-             transforms.ToTensor(),
-             transforms.Normalize((0.5),
-                                  (0.5))])
-    img_tensor=trans(sk2edge_pil)
-
-    label_tensor = img_tensor.unsqueeze(0).half()
-    inst_tensor = torch.Tensor([0.])
-    img_tensor = torch.Tensor([0.])
-
-    res_tensor = model.inference(label_tensor, inst_tensor, img_tensor)
-    res_arr = tensor2im(res_tensor[0])
-    mask_arr = np.array(mask_pil.resize([800, 800], Image.NEAREST))
-    mask_arr_bin = (mask_arr > 128).astype(np.uint8)
-    res_arr = res_arr*mask_arr_bin + (1-mask_arr_bin)*255
-
-    Image.fromarray(res_arr).resize(img_pil.size).save(f"./image/{exp_name}/{img}")
+def get_crop_bbox(mask_arr):
+    y, x = np.nonzero(mask_arr)
+    ymin = np.min(y)
+    ymax = np.max(y)
+    xmin = np.min(x)
+    xmax = np.max(x)
+    bbox = [max(xmin - 20, 0),
+            max(ymin - 20, 0),
+            min(xmax + 20, mask_arr.shape[1]),
+            min(ymax + 20, mask_arr.shape[0])]
+    return bbox
 
 
+def get_mask_whitebg(img_arr):
+    img_arr = (img_arr > 128).astype(np.uint8)*255
+    h, w= img_arr.shape[:2]
+    img_tmp = np.ones((h+2, w+2))*255
+    img_tmp[1:-1, 1:-1] = img_arr
+    mask = flood_fill(img_tmp/255., (0, 0), -1, tolerance=0.1)
+    mask_bin = (mask[1:-1, 1:-1] >= 0).astype(np.uint8)
+    return mask_bin
+
+def skeleton(gray, line=128):
+    binary = (gray < line).astype(np.uint8)
+    skeleton0 = morphology.skeletonize(binary)
+    skeleton = skeleton0.astype(np.uint8)*255
+    return skeleton
+
+def sketch2edge(sketch_arr, binary_line):
+    # skeleton and set the line size 3
+    sk1_arr = skeleton(sketch_arr, binary_line)
+    sk1_pil = Image.fromarray(255-sk1_arr).filter(ImageFilter.MinFilter(size=3))
+    mask_bin = get_mask_whitebg(np.array(sk1_pil))
+
+    # Make the two edge sketch
+    sk2_pil = sk1_pil.filter(ImageFilter.MinFilter(size=5))
+    sk2_arr_bin = np.array(sk2_pil) > 128
+    sk1_arr_bin = np.array(sk1_pil) > 128
+    sknew_arr_bin = ((~sk2_arr_bin) & sk1_arr_bin).astype(np.uint8)
+
+    mask_arr = mask_bin*128 + (1-mask_bin)*255
+    img_arr = mask_arr*sknew_arr_bin
+    return img_arr, mask_bin*255
+
+
+class FoldModel(object):
+    def __init__(self):
+        opt = Namespace(name='sketchshade0529', gpu_ids=[0], checkpoints_dir='./checkpoints', model='pix2pixHD',
+                        norm='instance', use_dropout=False, data_type=16, verbose=False, fp16=False,
+                        local_rank=0, batchSize=1, loadSize=800, fineSize=800, label_nc=0, input_nc=1, output_nc=1,
+                        resize_or_crop='resize', serial_batches=True, netG='global', ngf=64, n_downsample_global=4,
+                        n_blocks_global=9, n_blocks_local=3, n_local_enhancers=1, niter_fix_global=0, no_instance=True,
+                        instance_feat=False, label_feat=False, feat_num=3, load_features=False, n_downsample_E=4, nef=16,
+                        n_clusters=10, aspect_ratio=1.0, phase='test', which_epoch='20', how_many=-1,
+                        cluster_path='features_clustered_010.npy', use_encoded_image=False,  isTrain=False)
+        self.model = InferenceModel()
+        self.model.initialize(opt)
+        self.model.half()
+        self.trans = transforms.Compose([transforms.ToTensor(),
+                                         transforms.Normalize((0.5),
+                                                              (0.5))])
+
+    def predict_(self, sketch_pil):
+        raw_size = sketch_pil.size
+        sketch_pil = sketch_pil.resize([800, 800])
+        sketch_arr = np.array(sketch_pil)
+
+        sk2edge_arr, mask_arr = sketch2edge(sketch_arr, 224)
+        sk2edge_pil = Image.fromarray(sk2edge_arr)
+        #sk2edge_pil.save("sk2edge_pil.png")
+        #Image.fromarray(mask_arr).save("mask.png")
+        img_tensor = self.trans(sk2edge_pil)
+
+        label_tensor = img_tensor.unsqueeze(0).half()
+        inst_tensor = torch.Tensor([0.])
+        img_tensor = torch.Tensor([0.])
+
+        res_tensor = self.model.inference(label_tensor, inst_tensor, img_tensor)
+        res_arr = tensor2im(res_tensor[0])
+
+        mask_arr_bin = (mask_arr > 128).astype(np.uint8)
+        res_arr = res_arr * mask_arr_bin + (1 - mask_arr_bin) * 255
+        res_pil = Image.fromarray(res_arr).resize(raw_size)
+        return res_pil
+
+    def predict(self, img_path):
+        img_pil = Image.open(img_path).convert('L')
+        raw_size = img_pil.size
+        res_img = Image.fromarray(
+            np.ones((raw_size[1], raw_size[0]), dtype=np.uint8) * 255)
+        mask_bin = get_mask_whitebg(np.array(img_pil))
+        bbox = get_crop_bbox(mask_bin)
+        img_pil_crop = img_pil.crop(bbox)
+        res_pil = self.predict_(img_pil_crop)
+        res_img.paste(res_pil, bbox)
+        return res_img
+
+
+def test_single():
+    #img_path = "./20211119-142037A.png"
+    #mask_path = "./20211119-142037A_mask.png"
+    img_path = "./image/test4.jpeg"
+    model = FoldModel()
+    res_pil = model.predict(img_path)
+    res_pil.save('out.png')
+
+
+def test_exp():
+    exp_name = "sketch_test_white"
+    os.makedirs(f"image/{exp_name}", exist_ok=True)
+    base_path = f"/data/Dataset/sketch_test/{exp_name}"
+    imgs = os.listdir(f"{base_path}/sketch")
+
+    model = FoldModel()
+    for img in imgs:
+        img_path = f"{base_path}/sketch/{img}"
+        res_img = model.predict(img_path)
+        res_img.save(f"./image/{exp_name}/{img}")
+
+test_single()
+#test_exp()
